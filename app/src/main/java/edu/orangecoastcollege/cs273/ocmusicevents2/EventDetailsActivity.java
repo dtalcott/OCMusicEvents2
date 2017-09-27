326de5696eaf8e68ed69d06b3a9671e6c9630f74
Tallcott <dtallcott@student.cccd.edu>
2017-09-26 17:00:17 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventDetailsActivity.java
@@ -0,0 +1,71 @@
+package edu.orangecoastcollege.cs273.ocmusicevents2;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("Title");
+        String date = intent.getStringExtra("Date");
+        String day = intent.getStringExtra("Day");
+        String time = intent.getStringExtra("Time");
+        String location = intent.getStringExtra("Location");
+        String address1 = intent.getStringExtra("Address1");
+        String address2 = intent.getStringExtra("Address2");
+        //String details = intent.getStringExtra("Details");
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+
+        ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
+        TextView eventTitleTextView = (TextView) findViewById(R.id.eventTitleTextView);
+        TextView eventDateTextView = (TextView) findViewById(R.id.eventDateTextView);
+        TextView eventDayTextView = (TextView) findViewById(R.id.eventDayTextView);
+        TextView eventTimeTextView = (TextView) findViewById(R.id.eventTimeTextView);
+        TextView eventLocationTextView = (TextView) findViewById(R.id.eventLocationTextView);
+        TextView eventAddress1TextView = (TextView) findViewById(R.id.eventAddress1TextView);
+        TextView eventAddress2TextView = (TextView) findViewById(R.id.eventAddress2TextView);
+        //TextView eventDetailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+
+        AssetManager am = this.getAssets();
+        try {
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        }
+        catch (IOException e)
+        {
+            Log.e("OC Music Events", "Error loading image: " + imageFileName, e);
+        }
+        eventTitleTextView.setText(title);
+        eventDateTextView.setText(date);
+        eventDayTextView.setText("- " + day);
+        eventTimeTextView.setText(time);
+        eventLocationTextView.setText(location);
+        eventAddress1TextView.setText(address1);
+        eventAddress2TextView.setText(address2);
+        //eventDetailsTextView.setText(details);
+
+
+    }
+
+    protected void goBackToList(View v)
+    {
+        finish();
+    }
+}
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ocmusicevents2/EventListAdapter.java
@@ -0,0 +1,76 @@
+package edu.orangecoastcollege.cs273.ocmusicevents2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ *
+ * @author Devon Tallcott
+ * @version 2.0
+ *
+ * Created on 9/26/2017.
+ */
+
+public class EventListAdapter extends ArrayAdapter<MusicEvent>
+{
+    private Context mContext;
+    private int mResource;
+    private List<MusicEvent> mAllEventsList;
+
+    //context = Activity that uses the adapter (EventListActivity)
+    //resource = Layout file to inflate (R.layout.music_event_list_item)
+    //objects = list of Music Events
+    public EventListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<MusicEvent> allMusicEvents)
+    {
+        super(context, resource, allMusicEvents);
+        mContext = context;
+        mResource = resource;
+        mAllEventsList = allMusicEvents;
+    }
+
+    //Need to override the method getView
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+       //Use "inflater" to inflate the custom layout (R.layout.music_event_list_item)
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        //Inflating the custom layout for one single item in the list
+        View listItemView = inflater.inflate(mResource, null);
+
+        ImageView listItemImageView = (ImageView) listItemView.findViewById(R.id.listItemImageView);
+        TextView listItemTitleTextView = (TextView) listItemView.findViewById(R.id.titleTextView);
+        TextView listItemDateTextView = (TextView) listItemView.findViewById(R.id.dateTextView);
+
+        MusicEvent selectedEvent = mAllEventsList.get(position);
+        listItemTitleTextView.setText(selectedEvent.getTitle());
+        listItemDateTextView.setText(selectedEvent.getDate());
+
+        //Use asset manager to retrieve the image
+        AssetManager am = mContext.getAssets();
+        try
+        {
+            InputStream stream = am.open(selectedEvent.getImageName());
+            Drawable image = Drawable.createFromStream(stream, selectedEvent.getTitle());
+            listItemImageView.setImageDrawable(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listItemView;
+    }
+}
